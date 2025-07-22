c870004a8b3181d66c420c8cdfa7090383568ee4
trainergav <41538515+trainergav@users.noreply.github.com>
2025-07-22 10:50:33 +1000

    Update install.sh

--- install.sh
+++ install.sh
@@ -107,6 +107,7 @@ fi
 
 mysql --user=root --password=$dbpassword -e "CREATE DATABASE $dbname DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;"
 mysql --user=root --password=$dbpassword -e "GRANT SELECT,INSERT,UPDATE,DELETE,CREATE,CREATE TEMPORARY TABLES,DROP,INDEX,ALTER ON  $dbname.* TO 'moodle'@'localhost' IDENTIFIED BY '$dbpassword';"
+mysql --user=root --password=$dbpassword -e "FLUSH PRIVILEGES;"
 
 # Set up the Moodle data folder.
 if [ ! -d "/var/www/$dbname" ]; then
