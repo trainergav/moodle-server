8787ced4a7b6d2db31e042cdf0b479fe588480b6
trainergav <41538515+trainergav@users.noreply.github.com>
2025-12-01 11:55:06 +1100

    Update install.sh

--- install.sh
+++ install.sh
@@ -8,7 +8,7 @@ copyOrDownload () {
     elif [ -f moodle-server/$1 ]; then
         cp moodle-server/$1 $2
     else
-        wget https://github.com/trainergav/moodle-server-501/raw/master/$1 -O $2
+        wget https://github.com/dhicks6345789/moodle-server/raw/master/$1 -O $2
     fi
     chmod $3 $2
 }
@@ -17,11 +17,15 @@ copyOrDownload () {
 moodlebranch="MOODLE_501_STABLE"
 servertitle="Moodle Server"
 sslhandler="none"
-dbname="moodle"
 
 # Read user-defined command-line flags.
 while test $# -gt 0; do
     case "$1" in
+        -moodlebranch)
+            shift
+            moodlebranch=$1
+            shift
+            ;;
         -servername)
             shift
             servername=$1
@@ -30,11 +34,6 @@ while test $# -gt 0; do
         -servertitle)
             shift
             servertitle=$1
-            shift
-            ;;
-         -dbname)
-            shift
-            dbname=$1
             shift
             ;;
         -dbpassword)
@@ -55,11 +54,10 @@ while test $# -gt 0; do
 done
 
 # Check all required flags are set, print a usage message if not.
-if [ -z "$servername" ] || [ -z "$dbpassword" ]; then 
-echo "Usage: install.sh -servername SERVERNAME -dbpassword DATABASEPASSWORD   -[dbname DBNAME] [-servertitle SERVERTITLE] [-sslhandler none | tunnel | caddy]"
+if [ -z "$servername" ] || [ -z "$dbpassword" ]; then
+    echo "Usage: install.sh -servername SERVERNAME -dbpassword DATABASEPASSWORD [-servertitle SERVERTITLE] [-sslhandler none | tunnel | caddy]"
     echo "SERVERNAME: The full domain name of the Moodle server (e.g. moodle.example.com)."
     echo "DATABASEPASSWORD: The root password to set for the MariaDB database."
-    echo "Optional: DBNAME: Install an instance of Moodle into a nominated database and location (e.g. \"moodle500\". Defaults to \"moodle\"" 
     echo "Optional: SERVERTITLE: A title for the Moodle server (e.g. \"My Company Moodle Server\". Defaults to \"Moodle Server\"" 
     echo "Optional: \"tunnel\" or \"caddy\" as SSL Handler options. If \"tunnel\", Moodle will be configured assuming an SSL tunneling"
     echo "          service (Cloudflare, NGrok, etc) will be used to provide SSL ingress. If \"caddy\", Caddy webserver will be installed"
@@ -75,16 +73,12 @@ if [ ! -d "/etc/apache2" ]; then
     rm /var/www/html/index.html
 fi
 
-echo Apache installed \""$servertitle"\"...
-
 # Make sure the MariaDB database server is installed.
 if [ ! -f "/usr/bin/mariadb" ]; then
     apt install -y mariadb-server
     # After installing MariaDB, it seems to be best practice to run the "mysql_secure_installation" script to reconfigure a few default settings to be more secure.
     # Here, we automate this process using the approach outlined at: https://bertvv.github.io/notes-to-self/2015/11/16/automating-mysql_secure_installation/
     mysql --user=root -e "UPDATE mysql.user SET Password=PASSWORD('$dbpassword') WHERE User='root';"
-    mysql --user=root --password=$dbpassword -e "GRANT ALL PRIVILEGES ON *.* TO 'root@'localhost';" 
-    mysql --user=root --password=$dbpassword -e "FLUSH PRIVILEGES;"
     mysql --user=root --password=$dbpassword -e "DELETE FROM mysql.user WHERE User='';"
     mysql --user=root --password=$dbpassword -e "DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('localhost', '127.0.0.1', '::1');"
     mysql --user=root --password=$dbpassword -e "DROP DATABASE IF EXISTS test;"
@@ -92,29 +86,20 @@ if [ ! -f "/usr/bin/mariadb" ]; then
     mysql --user=root --password=$dbpassword -e "FLUSH PRIVILEGES;"
 fi
 
-echo Database created \""$servertitle"\"...
-
 # Make sure PHP is installed.
 if [ ! -d "/etc/php" ]; then
     apt install -y php libapache2-mod-php php-mysql php-xml php-mbstring php-curl php-zip php-gd php-intl php-soap
-    sed -i 's/;max_input_vars = 1000/max_input_vars = 9000/g' /etc/php/8.4/apache2/php.ini
-#    sed -i 's/;max_execution_time = /max_execution_time = 160/g' /etc/php/8.4/apache2/php.ini
-#    sed -i 's/;max_input_vars = 1000/max_input_vars = 9000/g' /etc/php/8.4/apache2/php.ini
+    sed -i 's/;max_input_vars = 1000/max_input_vars = 6000/g' /etc/php/8.4/apache2/php.ini
 fi
 
-# Get Moodle 5.0.1 via Git.
+# Get Moodle via Git.
 if [ ! -d "moodle" ]; then
     git clone -b $moodlebranch git://git.moodle.org/moodle.git
 fi
 
-echo Moodle downloaded \""$servertitle"\"...
-
 # Create / set up the Moodle database.
-mysql --user=root --password=$dbpassword -e "CREATE DATABASE $dbname DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;"
-mysql --user=root --password=$dbpassword -e "GRANT SELECT,INSERT,UPDATE,DELETE,CREATE,CREATE TEMPORARY TABLES,DROP,INDEX,ALTER ON  $dbname.* TO 'moodleuser'@'localhost' IDENTIFIED BY '$dbpassword';"
-mysql --user=root --password=$dbpassword -e "FLUSH PRIVILEGES;"
-
-echo Moodle user database created and user setup \""$servertitle"\"...
+mysql --user=root --password=$dbpassword -e "CREATE DATABASE moodle DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;"
+mysql --user=root --password=$dbpassword -e "GRANT SELECT,INSERT,UPDATE,DELETE,CREATE,CREATE TEMPORARY TABLES,DROP,INDEX,ALTER ON moodle.* TO 'moodleuser'@'localhost' IDENTIFIED BY '$dbpassword';"
 
 # Set up the Moodle data folder.
 if [ ! -d "/var/www/moodle" ]; then
@@ -122,17 +107,12 @@ if [ ! -d "/var/www/moodle" ]; then
     chown www-data:www-data /var/www/moodle
 fi
 
-echo Moodle DATADIR created \""$servertitle"\"...
-
 # Copy the Moodle code to the web server.
 cp -r moodle/* /var/www/html
 rm /var/www/html/config-dist.php
 copyOrDownload config.php /var/www/html/config.php 0644
 sed -i "s/{{DBPASSWORD}}/$dbpassword/g" /var/www/html/config.php
 sed -i "s/{{SERVERNAME}}/$servername/g" /var/www/html/config.php
-
-echo Moodle config.php DBname and DB password updated \""$servertitle"\"...
-
 if [ $sslhandler = "tunnel" ] || [ $sslhandler = "caddy" ]; then
     sed -i "s/{{SSLPROXY}}/true/g" /var/www/html/config.php
 else
@@ -145,7 +125,7 @@ if [ ! -f "/usr/bin/dos2unix" ]; then
 fi
 
 # Set up Crontab if it doesn't already exist.
-if [ ! -f "/var/spool/cron/crontabs/www-data" ]; then
+if [ ! -f "/var/spool/cron/crontabs/root" ]; then
     copyOrDownload crontab crontab 0644
     dos2unix crontab
     crontab crontab
